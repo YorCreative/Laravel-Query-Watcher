f742a4b636ddd657d4da0e49f58561558fdded72
Yorda <hello@yorda.email>
2022-08-30 19:14:27 -0600

    Ignorables (#7)
    * Added ability to ignore specific tables and sql statements. 
    
    * Updated readme information for configuration.

--- src/QueryWatcher.php
+++ src/QueryWatcher.php
@@ -2,6 +2,7 @@
 
 namespace YorCreative\QueryWatcher;
 
+use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use YorCreative\QueryWatcher\Events\QueryEvent;
@@ -18,7 +19,14 @@ class QueryWatcher
                 $time_exceeds_ms_enabled = self::timeExceedsMsEnabled();
 
                 if ($time_exceeds_ms_enabled && self::getTimeExceedsMs() < $query->time
-                    || ! $time_exceeds_ms_enabled) {
+                    || ! $time_exceeds_ms_enabled
+                    && ! self::ignorable($query)) {
+                    // only capture queries if:
+                    //     - time_exceeds_ms is not enabled
+                    //     - time_exceeds_ms is enabled and query time exceeds the threshold.
+                    //     - query isn't on table that is ignorable.
+                    //     - query isn't a sql statement that is ignorable
+
                     event(new QueryEvent($query));
                 }
             });
@@ -43,5 +51,28 @@ class QueryWatcher
     public static function getTimeExceedsMs(): ?float
     {
         return config('querywatcher.scope.time_exceeds_ms.threshold') ?? 0;
+    }
+
+    /**
+     * @param  QueryExecuted  $queryExecuted
+     * @return bool
+     */
+    public static function ignorable(QueryExecuted $queryExecuted): bool
+    {
+        // check if query is on table that is ignorable
+        foreach (Config::get('querywatcher.scope.ignorable_tables') as $table) {
+            if (preg_match('~(from "'.$table.'"|update "'.$table.'"|into "'.$table.'")~i', $queryExecuted->sql)) {
+                return true;
+            }
+        }
+
+        // check if query includes ignorable statement
+        foreach (Config::get('querywatcher.scope.ignorable_statements') as $statement) {
+            if (str_starts_with(strtolower($queryExecuted->sql), strtolower($statement))) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
